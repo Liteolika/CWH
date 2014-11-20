8babfe1c09cb4de4bf752faae1d732c4526156ea
LiteOlika <peter@liteolika.se>
2014-11-20 22:20:03 +0100

    Initial commit

---
+++ CWH.Main/WordlistRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CWH.Main
+{
+    public class WordlistRepository
+    {
+
+
+        private List<WordListEntry> wordList;
+
+        public static WordlistRepository Create()
+        {
+            return new WordlistRepository();
+        }
+
+        public WordlistRepository()
+        {
+            this.wordList = new List<WordListEntry>();
+        }
+
+        public void AddWord(WordListEntry word)
+        {
+            if (wordList.Where(x => x.Word == word.Word).Any())
+                throw new ArgumentException("word already exists");
+
+            wordList.Add(word);
+        }
+
+        public WordListEntry GetWord(WordListEntry word)
+        {
+            return wordList.Where(x => x.Word == word.Word).FirstOrDefault();
+        }
+
+        public List<WordListEntry> FindWord(string word, bool searchPartial)
+        {
+
+            string pattern = "";
+            if (searchPartial)
+            {
+                pattern = @"^.*" + word + ".*$";
+            }
+            else
+            {
+                pattern = @"^" + word + "$";
+            }
+            Regex regex = new Regex(pattern);
+
+            var result = wordList.Where(x => regex.IsMatch(x.Word)).ToList();
+            return wordList.Where(x => regex.IsMatch(x.Word)).ToList();
+        }
+
+        public List<WordListEntry> FindWord(string word, bool searchPartial, char wildcard)
+        {
+            string pattern = "";
+
+            string wc = wildcard.ToString().Trim();
+
+            if (string.IsNullOrEmpty(wc))
+                throw new ArgumentException("invalid wildcard character");
+
+            pattern = word.Replace(wildcard.ToString(), ".");
+
+            if (searchPartial)
+            {
+                pattern = ".*" + pattern + ".*";
+            }
+            
+            pattern = "^" + pattern + "$";
+
+            var rex = new Regex(pattern);
+
+            return wordList.Where(x => rex.IsMatch(x.Word)).ToList();
+
+        }
+
+        public void RemoveWord(WordListEntry wordentry)
+        {
+            wordList.Remove(wordentry);
+        }
+
+    }
+}
---
+++ CWH.Tests/WordListRepo_Tests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CWH.Main;
+using System.Text.RegularExpressions;
+
+namespace CWH.Tests
+{
+    [TestFixture]
+    public class WordListRepo_Tests
+    {
+
+        private WordlistRepository wordlist;
+        private WordListEntry wordentry;
+
+        [SetUp]
+        public void Setup()
+        {
+            wordlist = CWH.Main.WordlistRepository.Create();
+            wordentry = new WordListEntry
+            {
+                Word = "starprogrammer"
+            };
+            wordlist.AddWord(wordentry);
+        }
+
+        [Test]
+        public void CanCreateRepo()
+        {
+            //var repo = WordlistRepository.Create();
+            Assert.IsInstanceOf(typeof(WordlistRepository), wordlist);
+        }
+
+        [Test]
+        public void CanAddWordToRepo()
+        {
+            wordlist.AddWord(new WordListEntry { Word = "n" });
+
+        }
+
+        [Test]
+        public void CanRetrieveWordFromRepo()
+        {
+            var retrievedWord = wordlist.GetWord(wordentry);
+            Assert.AreEqual(wordentry, retrievedWord);
+        }
+
+        [Test]
+        public void CannotAddDuplicateWord()
+        {
+            Assert.Throws(typeof(ArgumentException), delegate
+            {
+                wordlist.AddWord(wordentry);
+            });
+            
+        }
+
+        [Test]
+        public void CanSearchWord_Complete()
+        {
+            var retrievedWords = wordlist.FindWord(wordentry.Word, searchPartial: false);
+
+            Assert.Contains(wordentry, retrievedWords);
+
+        }
+
+        [Test]
+        public void CanSearchWord_Partial()
+        {
+            var retrievedWords = wordlist.FindWord("ramm", searchPartial: true);
+            Assert.Contains(wordentry, retrievedWords);
+        }
+
+        [Test]
+        public void CanSearchWord_Wildcards_One()
+        {
+            char wildcardChar = '*';
+            var retrievedWords = wordlist.FindWord(
+                "*gr*mme*", 
+                searchPartial: true,
+                wildcard: wildcardChar);
+
+            Assert.Contains(wordentry, retrievedWords);
+
+        }
+
+        [Test]
+        public void CanSearchWord_Wildcards_Two()
+        {
+            char wildcardChar = '*';
+            
+            var retrievedWords = wordlist.FindWord(
+                "starpr*gr*mme*",
+                searchPartial: true,
+                wildcard: wildcardChar);
+
+            Assert.Contains(wordentry, retrievedWords);
+
+        }
+
+        [Test]
+        public void ThrowsOnInvalidWildCardChar()
+        {
+            char wildcardChar = ' ';
+
+            Assert.Throws(typeof(ArgumentException), delegate
+            {
+                var retrievedWords = wordlist.FindWord(
+                "starpr*gr*mme*",
+                searchPartial: true,
+                wildcard: wildcardChar);
+            });
+
+        }
+
+        [Test]
+        public void CanRemoveWordFromList()
+        {
+            wordlist.RemoveWord(wordentry);
+            Assert.IsNull(wordlist.GetWord(wordentry));
+        }
+
+    }
+}
